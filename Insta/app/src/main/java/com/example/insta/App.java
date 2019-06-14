dd2ab9a01bb4976885d68bc9158ef8cec49a7945
B.Harish <balajiharish4@gmail.com>
2019-06-14 10:28:48 +0530

    Implemented SignUp and Login Functionalities and after the click of the login button user will enter into the Welcome Activity.
    Also added backButtonPressedTwice Functionality.

--- Insta/app/src/main/java/com/example/insta/App.java
+++ Insta/app/src/main/java/com/example/insta/App.java
@@ -2,5 +2,19 @@ package com.example.insta;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("4X9cSmmbfkxoIc7fcOxei6KkzEi3UN6lEIzH1TZo")
+                // if defined
+                .clientKey("ECcTnlfEnTYu72XsfjYZXeTpFkb8YOKjHleREzrA")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
+    }
 }
